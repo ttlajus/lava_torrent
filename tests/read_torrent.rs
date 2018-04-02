9a7ab3ada30733d5f487f2d9302494f43c3d5253
ttlajus <40629580+ttlajus@users.noreply.github.com>
2018-04-02 01:32:29 -0500

    implemented BEP 9 & 27, minor doc revision

--- tests/read_torrent.rs
+++ tests/read_torrent.rs
@@ -63,6 +63,14 @@ fn read_from_bytes() {
                 parsed.info_hash(),
                 "778ce280b595e57780ff083f2eb6f897dfa4a4ee".to_string()
             );
+            assert_eq!(
+                parsed.magnet_link(),
+                "magnet:?xt=urn:btih:778ce280b595e57780ff083f2eb6f897dfa4a4ee\
+                 &dn=ubuntu-16.04.4-desktop-amd64.iso\
+                 &tr=http://torrent.ubuntu.com:6969/announce\
+                 &tr=http://ipv6.torrent.ubuntu.com:6969/announce"
+                    .to_string(),
+            );
         }
         Err(_) => assert!(false),
     }
@@ -152,6 +160,15 @@ fn read_from_bytes_multiple_files() {
             assert_eq!(
                 parsed.info_hash(),
                 "a2a8d9b1ba0b1ac3d1ffa8062e02c0f9c23de31a".to_string()
+            );
+            assert_eq!(
+                parsed.magnet_link(),
+                "magnet:?xt=urn:btih:a2a8d9b1ba0b1ac3d1ffa8062e02c0f9c23de31a\
+                 &dn=tails-amd64-3.6.1\
+                 &tr=udp://tracker.torrent.eu.org:451\
+                 &tr=udp://tracker.coppersurfer.tk:6969\
+                 &tr=http://linuxtracker.org:2710/00000000000000000000000000000000/announce"
+                    .to_string(),
             );
         }
         Err(_) => assert!(false),
