d457bf655799260ca269b712d9f62005649ada12
ttlajus <40629580+ttlajus@users.noreply.github.com>
2018-04-12 21:24:58 -0500

    update integration tests for symlinks

--- tests/build_torrent.rs
+++ tests/build_torrent.rs
@@ -8,7 +8,7 @@ use lava_torrent::bencode::BencodeElem;
 use lava_torrent::torrent::v1::{Integer, Torrent, TorrentBuilder};
 
 const OUTPUT_ROOT: &str = "tests/tmp/";
-const PIECE_LENGTH: Integer = 32 * 1024; // 1024 KiB
+const PIECE_LENGTH: Integer = 32 * 1024; // n * 1024 KiB
 
 fn rand_file_name() -> String {
     OUTPUT_ROOT.to_string() + &rand::thread_rng().gen::<u16>().to_string()
@@ -63,7 +63,7 @@ fn build_multi_file_ok() {
     builder
         .add_extra_field(
             "creation date".to_string(),
-            BencodeElem::Integer(1523449884),
+            BencodeElem::Integer(1523607302),
         )
         .add_extra_field(
             "encoding".to_string(),
@@ -117,12 +117,12 @@ fn build_private() {
     // weird things when `builder` is not declared separately
     builder
         .add_extra_field(
-            "created by".to_string(),
-            BencodeElem::String("qBittorrent v4.0.4".to_string()),
+            "creation date".to_string(),
+            BencodeElem::Integer(1523607445),
         )
         .add_extra_field(
-            "creation date".to_string(),
-            BencodeElem::Integer(1522892270),
+            "encoding".to_string(),
+            BencodeElem::String("UTF-8".to_string()),
         )
         .set_privacy(true)
         .build()
@@ -130,26 +130,48 @@ fn build_private() {
         .write_into_file(&output_name)
         .unwrap();
 
-    assert!(Torrent::read_from_file(output_name).unwrap().is_private());
+    // compare against a sample file created by Deluge
+    assert_eq!(
+        Torrent::read_from_file(output_name).unwrap(),
+        Torrent::read_from_file("tests/samples/files-private.torrent").unwrap(),
+    );
 }
 
 #[test]
 fn build_symbolic_link() {
+    let output_name = rand_file_name() + ".torrent";
     // `canonicalize()` follows symbolic links, so we have to do a
     // `push()` separately to avoid that resolution
     let mut path = PathBuf::from("tests/files").canonicalize().unwrap();
     path.push("symlink");
 
-    let result = TorrentBuilder::new(
+    let builder = TorrentBuilder::new(
         "udp://tracker.coppersurfer.tk:6969/announce".to_string(),
         path,
         PIECE_LENGTH,
-    ).build();
+    );
+    // create a new chain because rustfmt does
+    // weird things when `builder` is not declared separately
+    builder
+        .add_extra_field(
+            "creation date".to_string(),
+            BencodeElem::Integer(1523607602),
+        )
+        .add_extra_field(
+            "encoding".to_string(),
+            BencodeElem::String("UTF-8".to_string()),
+        )
+        .add_extra_info_field("private".to_string(), BencodeElem::Integer(0))
+        .build()
+        .unwrap()
+        .write_into_file(&output_name)
+        .unwrap();
 
-    match result {
-        Ok(_) => assert!(false),
-        Err(e) => assert_eq!(e.kind(), ErrorKind::TorrentBuilderFailure),
-    }
+    // compare against a sample file created by Deluge
+    assert_eq!(
+        Torrent::read_from_file(output_name).unwrap(),
+        Torrent::read_from_file("tests/samples/symlink.torrent").unwrap(),
+    );
 }
 
 #[test]
